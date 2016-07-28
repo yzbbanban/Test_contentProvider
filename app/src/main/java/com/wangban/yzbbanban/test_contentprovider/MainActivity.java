d84e1092823213aef6551eb425d9306011cd9b87
yzbbanban <yzbbanban@live.com>
2016-07-28 21:56:46 +0800

    联系人查询

--- app/src/main/java/com/wangban/yzbbanban/test_contentprovider/MainActivity.java
+++ app/src/main/java/com/wangban/yzbbanban/test_contentprovider/MainActivity.java
@@ -1,5 +1,7 @@
 package com.wangban.yzbbanban.test_contentprovider;
 
+import android.database.Cursor;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -17,6 +19,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         contactListView = (ListView) findViewById(R.id.lv_contacts);
-        
+        contactList = setData();
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, contactList);
+        contactListView.setAdapter(adapter);
+    }
+
+    private List<String> setData() {
+        Cursor cursor = null;
+        try {
+            cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+            while (cursor.moveToNext()) {
+                String dispalyName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                String number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                contactList.add(dispalyName + "\n" + number);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+
+
+        return contactList;
     }
 }
